47c14085c769a6fa73cb528702e50d9b3f6a6d0b
李嘉俊 <2383690354@qq.com>
2025-03-06 18:43:22 +0800

    添加OpenApi

--- XAUAT.EduApi/Program.cs
+++ XAUAT.EduApi/Program.cs
@@ -26,11 +26,7 @@ builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
