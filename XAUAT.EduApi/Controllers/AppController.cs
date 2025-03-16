7b82ae0619bfd9a8f46ef0025fe42a07f6a5b2cd
李嘉俊 <2383690354@qq.com>
2025-03-16 10:41:30 +0800

    加入App更新

---
+++ XAUAT.EduApi/Controllers/AppController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XAUAT.EduApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AppController(IHttpClientFactory httpClientFactory)
+    : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetTag(string? token)
+    {
+        if (string.IsNullOrEmpty(token))
+        {
+            token = Environment.GetEnvironmentVariable("GITEE_ACCESS_TOKEN", EnvironmentVariableTarget.Process);
+        }
+
+        using var client = httpClientFactory.CreateClient();
+        var response = await client.GetAsync(
+            $"https://gitee.com/api/v5/repos/luckyfishisdashen/iOSClub.AppMobile/releases?access_token={token}&page=1&per_page=1&direction=desc");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return StatusCode((int)response.StatusCode, "获取标签失败");
+        }
+
+        var jsonString = await response.Content.ReadAsStringAsync();
+        return Ok(jsonString);
+    }
+}
