7539957e0bb315c3558921d0cd250f3ff9aac0db
李嘉俊 <2383690354@qq.com>
2025-03-18 19:05:43 +0800

    加入校车数据

---
+++ XAUAT.EduApi/Controllers/BusController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using XAUAT.EduApi.Models;
+
+namespace XAUAT.EduApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BusController(IHttpClientFactory httpClientFactory)
+    : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetBus(string? time)
+    {
+        var client = httpClientFactory.CreateClient();
+        time ??= DateTime.Today.ToString("yyyy-MM-dd");
+        var response =
+            await client.GetAsync(
+                $"https://school-bus.xauat.edu.cn/api/school/bus/user/runPlanPage?current=1&size=20&keyWord=&lineId=&date={time}");
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        var busModel = new BusModel();
+        var json = JObject.Parse(content);
+        busModel.Total = json["data"]!["total"]!.ToObject<int>();
+        foreach (var j in json["data"]!["records"]!)
+        {
+            busModel.Records.Add(new BusItem()
+            {
+                LineName = j["lineName"]!.ToString(),
+                Description = j["descr"]!.ToString(),
+                DepartureStation = j["departureStation"]!.ToString(),
+                ArrivalStation = j["arrivalStation"]!.ToString(),
+                RunTime = j["runTime"]!.ToString(),
+                ArrivalStationTime = j["arrivalStationTime"]!.ToString()
+            });
+        }
+        
+        return Ok(busModel);
+    }
+}
---
+++ XAUAT.EduApi/Models/BusModel.cs
@@ -0,0 +1,19 @@
+﻿namespace XAUAT.EduApi.Models;
+
+[Serializable]
+public class BusModel
+{
+    public List<BusItem> Records { get; set; } = [];
+    public int Total { get; set; }
+}
+
+[Serializable]
+public class BusItem
+{
+    public string LineName { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string DepartureStation { get; set; } = "";
+    public string ArrivalStation { get; set; } = "";
+    public string RunTime { get; set; } = "";
+    public string ArrivalStationTime { get; set; } = "";
+}
