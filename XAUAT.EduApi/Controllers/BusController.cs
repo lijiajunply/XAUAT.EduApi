e664a794d4d1ae2b85e5f5361c5e290fc933321a
李嘉俊 <2383690354@qq.com>
2025-06-04 01:37:34 +0800

    加入新平台数据

--- XAUAT.EduApi/Controllers/BusController.cs
+++ XAUAT.EduApi/Controllers/BusController.cs
@@ -13,8 +13,75 @@ public class BusController(IHttpClientFactory httpClientFactory, IConnectionMult
 {
     private readonly IDatabase _redis = muxer.GetDatabase();
 
+    /// <summary>
+    /// 新数据平台
+    /// </summary>
+    /// <param name="loc">All,雁塔,草堂</param>
+    /// <param name="time"></param>
+    /// <returns></returns>
     [HttpGet("{time?}")]
-    public async Task<IActionResult> GetBus(string? time)
+    public async Task<IActionResult> GetBusFromNewData(string? time, string loc = "ALL")
+    {
+        using var client = httpClientFactory.CreateClient();
+        time ??= DateTime.Today.ToString("yyyy-MM-dd");
+
+        var key = "bus_new_data:" + time;
+
+        var bus = await _redis.StringGetAsync(key);
+
+        if (bus.HasValue)
+        {
+            return Ok(JsonConvert.DeserializeObject<BusModel>(bus.ToString()));
+        }
+
+        var response =
+            await client.PostAsJsonAsync(
+                "https://bcdd.xauat.edu.cn/api/openapi/getDayBusPlans", new { type = loc, nowDay = time });
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var busModel = new BusModel();
+        var json = JObject.Parse(content);
+
+        var i = json["data"]!["dfBusPlans"] as JArray;
+
+        if (i?.Count == 0)
+        {
+            return Ok(await GetBusFromOldData(time));
+        }
+
+        foreach (var j in json["data"]!["dfBusPlans"]!)
+        {
+            if (j["frcamp"] != null && !string.IsNullOrEmpty(j["frcamp"]!.ToString())) continue;
+            var departure = j["fscamp"] + "校区";
+            var arrival = j["fecamp"] + "校区";
+            var timestamp = long.Parse(j["fstime"]!.ToString()) * 10000;
+
+            var tricks1970 = new DateTime(1970, 1, 1, 8, 0, 0).Ticks; //1970年1月1日刻度
+            var time_tricks = tricks1970 + timestamp; //日志日期刻度
+
+            var runTime = new DateTime(time_tricks);
+            busModel.Records.Add(new BusItem()
+            {
+                LineName = $"{departure}→{arrival}",
+                Description = j["fbusNo"]!.ToString(),
+                DepartureStation = departure,
+                ArrivalStation = arrival,
+                RunTime = runTime.ToString("T"),
+                ArrivalStationTime = "01:30",
+            });
+        }
+
+        busModel.Total = busModel.Records.Count;
+
+        if (busModel.Total != 0)
+            await _redis.StringSetAsync(key, JsonConvert.SerializeObject(busModel),
+                expiry: new TimeSpan(0, 12, 0, 0));
+
+        return Ok(busModel);
+    }
+
+    private async Task<BusModel> GetBusFromOldData(string? time)
     {
         using var client = httpClientFactory.CreateClient();
         time ??= DateTime.Today.ToString("yyyy-MM-dd");
@@ -23,7 +90,7 @@ public class BusController(IHttpClientFactory httpClientFactory, IConnectionMult
 
         if (bus.HasValue)
         {
-            return Ok(JsonConvert.DeserializeObject<BusModel>(bus.ToString()));
+            return JsonConvert.DeserializeObject<BusModel>(bus.ToString()) ?? new BusModel();
         }
 
         var response =
@@ -40,7 +107,7 @@ public class BusController(IHttpClientFactory httpClientFactory, IConnectionMult
             busModel.Records.Add(new BusItem()
             {
                 LineName = j["lineName"]!.ToString(),
-                Description = j["descr"]!.ToString(),
+                Description = j["descr"]! + " (调用的为旧平台数据)",
                 DepartureStation = j["departureStation"]!.ToString(),
                 ArrivalStation = j["arrivalStation"]!.ToString(),
                 RunTime = j["runTime"]!.ToString(),
@@ -52,6 +119,6 @@ public class BusController(IHttpClientFactory httpClientFactory, IConnectionMult
             await _redis.StringSetAsync("bus:" + time, JsonConvert.SerializeObject(busModel),
                 expiry: new TimeSpan(0, 12, 0, 0));
 
-        return Ok(busModel);
+        return busModel;
     }
 }
