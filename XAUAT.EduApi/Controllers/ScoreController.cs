0659c099e07778671a9135f365ca1d0c5bd67f6d
李嘉俊 <2383690354@qq.com>
2025-09-12 17:49:27 +0800

    再试一次

--- XAUAT.EduApi/Controllers/ScoreController.cs
+++ XAUAT.EduApi/Controllers/ScoreController.cs
@@ -43,7 +43,6 @@ public class ScoreController(
             }
 
             var html = await client.GetStringAsync(url);
-            Console.WriteLine(html);
             var result = new SemesterResult();
             result.Parse(html);
             return result;
