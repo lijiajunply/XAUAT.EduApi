010da7f3fbcd5844fac0d0675ba0ef38e2603db4
李嘉俊 <2383690354@qq.com>
2025-09-12 21:41:01 +0800

    删除CW

--- XAUAT.EduApi/Controllers/ScoreController.cs
+++ XAUAT.EduApi/Controllers/ScoreController.cs
@@ -44,7 +44,6 @@ public class ScoreController(
             }
 
             var html = await client.GetStringAsync(url);
-            Console.WriteLine(html);
             var result = new SemesterResult();
             result.Parse(html);
             return result;
