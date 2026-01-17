fe6107f340e02547479b150b76dd6e3e09a74204
LuckyFish at macOS <2383690354@qq.com>
2026-01-17 19:24:32 +0800

    refactor(core): 移除插件系统和事件总线功能
    - 删除 Plugins 目录下的所有文件 (IPlugin.cs, PluginManager.cs, PluginContext.cs)
    - 删除 Events 目录下的所有文件 (IEventBus.cs, EventBus.cs, IEvent.cs, IEventHandler.cs, EventBase.cs)
    - 从 ServiceCollectionExtensions.cs 中移除插件和事件驱动服务注册方法
    - 从 Program.cs 中移除插件系统初始化和加载代码
    - 移除相关 using 指令和依赖注入配置
    - 修改 PlaygroundController 以支持新的请求模式并添加超时控制
    - 更新 UserAgentRotator 以支持可空 HttpClient 类型

--- XAUAT.EduApi/Controllers/PlaygroundController.cs
+++ XAUAT.EduApi/Controllers/PlaygroundController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using XAUAT.EduApi.Services;
 
 namespace XAUAT.EduApi.Controllers;
 
@@ -8,8 +10,8 @@ namespace XAUAT.EduApi.Controllers;
 [Consumes("application/json")]
 public class PlaygroundController(IHttpClientFactory httpClientFactory) : ControllerBase
 {
-    [HttpGet("{url}")]
-    public async Task<IActionResult> Get(string url)
+    [HttpGet]
+    public async Task<IActionResult> Get()
     {
         var cookie = Request.Headers.Cookie.ToString();
         if (string.IsNullOrEmpty(cookie))
@@ -18,8 +20,11 @@ public class PlaygroundController(IHttpClientFactory httpClientFactory) : Contro
         }
         
         using var client = httpClientFactory.CreateClient();
+        client.SetRealisticHeaders();
+        client.Timeout = TimeSpan.FromSeconds(6); // 添加超时控制
         client.DefaultRequestHeaders.Add("Cookie", cookie);
-        var response = await client.GetAsync(url);
-        return Ok(await response.Content.ReadAsStringAsync());
+        var response = await client.GetAsync("https://swjw.xauat.edu.cn/student/for-std/credit-certification-apply/other_apply/get-all-course-module?programId=3241");
+        var content = await response.Content.ReadAsStringAsync();
+        return Ok(JObject.Parse(content));
     }
 }
